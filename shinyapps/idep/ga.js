8fbe9b6a2fcab849f9bda5c592590404aac2235c
Eun Woo Son <eunwoo.son@sdstate.edu>
2017-10-23 09:37:56 -0500

    update ga

--- shinyapps/idep/ga.js
+++ shinyapps/idep/ga.js
@@ -2,7 +2,12 @@
     (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
     m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  
-ga('create', 'UA-87863704-1', 'auto');
+
+//ShinyGO
+//ga('create', 'UA-87863704-1', 'auto');
+//iDEP at DO
+ga('create', 'UA-87863704-2', 'auto');
+//iDEP at prod
+//ga('create', 'UA-87863704-3', 'auto');
+
 ga('send', 'pageview');
-  
