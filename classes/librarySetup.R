bfbf27a1dadee0b5e833ad5d8af181667b199b81
Ge <Xijin.Ge@sdstate.edu>
2017-11-03 18:11:17 -0500

    Signed-off-by: Ge <Xijin.Ge@sdstate.edu>

--- classes/librarySetup.R
+++ classes/librarySetup.R
@@ -16,7 +16,7 @@ list.of.packages <- c(
 list.of.bio.packages  <- c(
   "limma", "DESeq2", "edgeR", "gage", "PGSEA", "fgsea", "ReactomePA", "pathview", "PREDA","impute", "runibic","QUBIC",
   "PREDAsampledata", "sfsmisc", "lokern", "multtest", "hgu133plus2.db", 
-   ,"org.Ag.eg.db","org.At.tair.db","org.Bt.eg.db","org.Ce.eg.db","org.Cf.eg.db",
+   "org.Ag.eg.db","org.At.tair.db","org.Bt.eg.db","org.Ce.eg.db","org.Cf.eg.db",
    "org.Dm.eg.db","org.Dr.eg.db","org.EcK12.eg.db","org.EcSakai.eg.db","org.Gg.eg.db",
    "org.Hs.eg.db","org.Hs.ipi.db","org.Mm.eg.db","org.Mmu.eg.db","org.Pf.plasmo.db",
    "org.Pt.eg.db","org.Rn.eg.db","org.Sc.sgd.db","org.Sco.eg.db","org.Ss.eg.db",
