18d2c860668b9c3f7ad6c8f3f29576220ab4614d
root <root@resapp003.jacks.local>
2018-01-03 17:12:19 -0500

    prod Server

---
+++ shinyapps/hello/ui.R
@@ -0,0 +1,25 @@
+library(shiny)
+
+# Define UI for application that plots random distributions 
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Hello Shiny!"),
+  
+  # Sidebar with a slider input for number of observations
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("obs", 
+                  "Number of observations:", 
+                  min = 1, 
+                  max = 1000, 
+                  value = 500)
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
+))
+     
