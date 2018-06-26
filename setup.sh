a46ed18f90de3941868ed0234de2a3cd20e5905a
HalforcNull <yaorunan@gmail.com>
2018-06-26 09:45:52 -0500

    remove temp file

--- setup.sh
+++ setup.sh
@@ -11,14 +11,19 @@ cd data
 
 wget https://sdsu.box.com/shared/static/c24f792ojoikpzu0lkpng8uuf9ychwm7.gz -O pathwayDB.tar.gz
 tar xvzf pathwayDB.tar.gz
+rm pathwayDB.tar.gz
 wget https://sdsu.box.com/shared/static/9v1ao6mwhduvrcx793j3answph9gqnkt.gz -O motif.tar.gz
 tar xvzf motif.tar.gz
+rm motif.tar.gz
 wget https://sdsu.box.com/shared/static/mns0k1uvwtfnsohoc89b984ih36nmnz9.gz -O geneInfo.tar.gz
 tar xvzf geneInfo.tar.gz
+rm geneInfo.tar.gz
 wget https://sdsu.box.com/shared/static/qwpdh36vcisgy1hcmadck8i8ezhvr2fh.gz -O data.go.tar.gz
 tar xvzf data.go.tar.gz
+rm data.go.tar.gz
 wget https://sdsu.box.com/shared/static/sorewt7w6iypmhg2k2xhyi8myeit156o.gz -O convertIDs.db.tar.gz
 tar xvzf convertIDs.db.tar.gz
+rm convertIDs.db.tar.gz
 
 echo 'Data has been downloaded and unziped'
 
