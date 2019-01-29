b1d5e2cde5b8df04a942879a7a0ca0738ff3e5d6
HalforcNull <yaorunan@gmail.com>
2019-01-29 14:08:22 -0600

    Create viewManager.R
    in case we need

---
+++ shinyapps/idep/views/viewManager.R
@@ -0,0 +1,44 @@
+#` View Manager
+#` This file contains a reference to any workable ui.
+#` 
+#` 
+#` 
+#` 
+#` 
+#` 
+#` 
+#` 
+#` 
+
+
+
+
+
+
+
+
+
+
+
+
+View.Check.UI <- function(){
+
+}
+
+View.Get.UI <- function(){
+
+}
+
+View.Get.UI.List <- function(){
+
+}
+
+
+
+
+
+
+
+
+
+
