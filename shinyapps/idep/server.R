deb54f58af363be7f820e5f5054ddf6c66d2a8fb
HalforcNull <yaorunan@gmail.com>
2019-02-04 12:46:08 -0600

    add data path into config file
    #46

--- shinyapps/idep/server.R
+++ shinyapps/idep/server.R
@@ -90,7 +90,7 @@ maxSamplesEDAplot = 100  # max number of samples for EDA plots
 ################################################################
 
 # relative path to data files
-datapath = "../../data/data92/"   # production server
+datapath = CONFIG_DATAPATH   # production server
 
 sqlite  <- dbDriver("SQLite")
 convert <- dbConnect( sqlite, paste0(datapath, "convertIDs.db"), flags=SQLITE_RO)  #read only mode
