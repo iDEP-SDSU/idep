ea7860b8f4e08decf35ecb5b4d6f2f07394aeda0
HalforcNull <yaorunan@gmail.com>
2019-02-13 14:22:13 -0600

    PreProcessing.Logic$RemoveAllMissingRows

--- shinyapps/iDepModified/businessLogic/PreProcessing.R
+++ shinyapps/iDepModified/businessLogic/PreProcessing.R
@@ -7,28 +7,38 @@ PreProcessing.Logic <- R6Class("PreProcessing.Logic")
 
 PreProcessing.Logic$set("public","ReadCountPreprocess",
 	function(rawData){
-		tmp.dataExcludeNonNumeric <- self$RemoveNonNumericalColumns(rawData)
-		tmp.dataExcludeAllMissingColumn <- 
-		tmp.dataRefineGeneId <-
-		tmp.dataRefineGeneId <-
-		tmp.dataAfterImpute <- 
+		tmp.data <- self$RemoveNonNumericalColumns(rawData)
+		tmp.data <- self$RemoveAllMissingRows(tmp.data)
+		tmp.data <- 
+		tmp.data <-
+		tmp.data <- 
 	}
 )
 
 PreProcessing.Logic$set("public", "RemoveNonNumericalColumns",
 	function(rawData){
-
+	# Remove non numeric column
 		isNumeric <- apply(rawData,2,is.numeric)
-		
+
 		if( sum(isNumeric) <= 1 ){
 			return(NULL)
 		}
 
-		isKeep <- isNumeric
-		isKeep[1] <- TRUE
+		idxKeep <- isNumeric
+		idxKeep[1] <- TRUE
 
-		return(rawData[,isKeep])
+		return(rawData[,idxKeep])
 	}
 )
+
+
+PreProcessing.Logic$set("public", "RemoveAllMissingRows",
+	## Remove rows with only missing value
+	function(dat){
+		idxKeep = which( apply(dat[,-1],1, function(y) sum( is.na(y) ) ) != dim(dat)[2]-1 )
+		return(dat[idxKeep,])
+	}
+)
+
 
 
