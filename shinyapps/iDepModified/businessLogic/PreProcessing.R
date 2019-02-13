d08aca9ea0a912e31b2c1b91b71e22bbe0f32718
HalforcNull <yaorunan@gmail.com>
2019-02-13 14:40:12 -0600

    SetGeneIDtoRowname()

--- shinyapps/iDepModified/businessLogic/PreProcessing.R
+++ shinyapps/iDepModified/businessLogic/PreProcessing.R
@@ -10,8 +10,9 @@ PreProcessing.Logic$set("public","ReadCountPreprocess",
 		tmp.data <- self$RemoveNonNumericalColumns(rawData)
 		tmp.data <- self$RemoveAllMissingRows(tmp.data)
 		tmp.data <- self$CleanGeneIDs(tmp.data)
+		tmp.data <- self$SetGeneIDtoRowname(tmp.data)
 		tmp.data <- self$CleanSampleNames(tmp.data)
-		tmp.data <- 
+		tmp.data <- self$SortBySD(tmp.data)
 	}
 )
 
@@ -36,6 +37,7 @@ PreProcessing.Logic$set("public", "RemoveAllMissingRows",
 	## Remove rows with only missing value
 	function(dat){
 		idxKeep = which( apply(dat[,-1],1, function(y) sum( is.na(y) ) ) != dim(dat)[2]-1 )
+		
 		return(dat[idxKeep,])
 	}
 )
@@ -43,18 +45,27 @@ PreProcessing.Logic$set("public", "RemoveAllMissingRows",
 
 PreProcessing.Logic$set("public", "CleanGeneIDs",
 	function(dat){
-	# remove spaces in gene ids
-	# remove " in gene ids, mess up SQL query				
-	# remove ' in gene ids		
-	# remove one or two digits after "." at the end.
-    # A35244.1 -> A35244  or A35244.23 -> A35244, but not more than two.  GLYMA.18G52160 stays the same.
+		# remove spaces in gene ids
+		# remove " in gene ids, mess up SQL query				
+		# remove ' in gene ids		
+		# remove one or two digits after "." at the end.
+    	# A35244.1 -> A35244  or A35244.23 -> A35244, but not more than two.  GLYMA.18G52160 stays the same.
 		dat[,1] <- toupper(dat[,1])
 		dat[,1] <- gsub(" |\"|\'|\\.[0-9]{1,2}$", "", dat[,1])
+
+		# remove duplicated genes
+		dat <- dat[!duplicated(dat[,1]) ,]
+
 		return(dat)
 	}
 )
 
-
+PreProcessing.Logic$set("public", "SetGeneIDtoRowname",
+	function(dat){	
+		rownames(dat) <- dat[,1]
+		return(as.matrix(dat[,c(-1)]))
+	}
+)
 
 
 
