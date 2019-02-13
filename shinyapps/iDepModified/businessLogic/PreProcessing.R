fafc6d2a29e1395b5ac5b2e3e189a5350f2a964b
HalforcNull <yaorunan@gmail.com>
2019-02-13 14:28:43 -0600

    PreProcessing.Logic$CleanGeneIDs

--- shinyapps/iDepModified/businessLogic/PreProcessing.R
+++ shinyapps/iDepModified/businessLogic/PreProcessing.R
@@ -9,8 +9,8 @@ PreProcessing.Logic$set("public","ReadCountPreprocess",
 	function(rawData){
 		tmp.data <- self$RemoveNonNumericalColumns(rawData)
 		tmp.data <- self$RemoveAllMissingRows(tmp.data)
-		tmp.data <- 
-		tmp.data <-
+		tmp.data <- self$CleanGeneIDs(tmp.data)
+		tmp.data <- self$CleanSampleNames(tmp.data)
 		tmp.data <- 
 	}
 )
@@ -39,6 +39,24 @@ PreProcessing.Logic$set("public", "RemoveAllMissingRows",
 		return(dat[idxKeep,])
 	}
 )
+
+
+PreProcessing.Logic$set("public", "CleanGeneIDs",
+	function(dat){
+	# remove spaces in gene ids
+	# remove " in gene ids, mess up SQL query				
+	# remove ' in gene ids		
+	# remove one or two digits after "." at the end.
+    # A35244.1 -> A35244  or A35244.23 -> A35244, but not more than two.  GLYMA.18G52160 stays the same.
+		dat[,1] <- toupper(dat[,1])
+		dat[,1] <- gsub(" |\"|\'|\\.[0-9]{1,2}$", "", dat[,1])
+		return(dat)
+	}
+)
+
+
+
+
 
 
 
