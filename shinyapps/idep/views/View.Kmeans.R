863f46746e6bb48a6ae7f551d87f9dc39f5b6615
HalforcNull <yaorunan@gmail.com>
2019-08-29 16:32:16 -0500

    todo: 1. AdditionalPopupTriggers, 2.downloadPlotSection, 3. PathwayEnrichmentConfigration, 4. pathway visualizatgion and download

--- shinyapps/idep/views/View.Kmeans.R
+++ shinyapps/idep/views/View.Kmeans.R
@@ -8,22 +8,37 @@ View.Kmeans <- R6Class("View.Kmeans")
 
 # Side Bar
 
-View.Heatmap$set("public", "sidebarPanel", 
+View.Kmeans$set("public", "sidebarPanel", 
 	function(){
 		sidebarPanel(
-			# not finish yet
+			self$clusterConfiguration(),
+			actionButton("btn_Kmeans_rerun", "Re-Run")
+			self$additionalPopupTriggers(),
+			self$downloadPlotSection(),
+			self$pathwayEnrichmentConfiguration(),
+			self$pathwayVisualizationAndDownload()
 		)
 	}
 )
 
-View.Heatmap$set("public", "mainPanel",
+# Main panel
+
+# Besid main plot, Kmeans page contains 5 different pop-out pages:
+# 1. Enriched TF binding motifs in promoters of Kmeans clusters, aka PopPromoters
+# 2. Determining the number of clusters (k), aka PopNumberOfClusters
+# 3. TSNE plot of genes, aka PopTSNEPlot
+# 4. Distribution of variations among genes, aka PopGeneDistribution
+# 5. Visualize enrichment, aka PopVisualizeEnrichment
+
+View.Kmeans$set("public", "mainPanel",
 	function(){
 		mainPanel(
-			plotOutput("KmeansHeatmap", inline=TRUE),
+			plotOutput("Kmeans_Heatmap", inline=TRUE),
 			br(),
-			self$EnrichedPathways()
-			br(),
-			self$PopMotifInPromoters(),
+			h4("Enriched pathways for each cluster"),
+			tableOutput("tbl_KmeansGO"),
+
+			self$PopPromoters(), 
 			self$PopNumberOfClusters(),
 			self$PopTSNEPlot(),
 			self$PopGeneDistribution(),
@@ -37,12 +52,126 @@ View.Heatmap$set("public", "mainPanel",
 ###################			Component Functions				###################
 ###############################################################################
 
+# Configuration which will affect heatmap change
+View.Kmeans$set("public", "clusterConfiguration",
+	function(){
+		wellPanel(
+			sliderInput(
+				"num_Kmeans_GenesKNN", 
+				label = h4("Most variable genes to include "),
+				min   = 0, 
+				max   = 12000, 
+				value = 2000,
+				step  = 100
+			),
+			sliderInput(
+				"num_Kmeans_Culsters",
+				label = h4("Number of Clusters"), 
+				min   = 2, 
+				max   = 20, 
+				value = 4,
+				step  = 1 
+			),
+			selectInput(
+				"kmeansNormalization", 
+				h5("Normalize by gene:"), 
+				choices = list(
+					"Mean center" = "geneMean",
+					"Standardization" = "geneStandardization",
+					"L1 Norm"	= "L1Norm"
+				),
+				selected = "Standardization"
+			)     
+		)
+	}
+)
+
+# 
 
 
 
+# Pop out windows definitions
+View.Kmeans$set("public", "PopPromoters",
+	function(){
+		bsModal(
+			"modal_Kmeans_ShowPromoters",
+			"Enriched TF binding motifs in promoters of Kmeans clusters",
+			"btn_Kmeans_showMotifKmeans",
+			size = "large",
+			radioButtons("select_PromoterKmeans", 
+                        label    = NULL, 
+                        choices  = list("Upstream 300bp as promoter" = 300, 
+                                        "Upstream 600bp as promoter" = 600),
+                        selected = 300),
+          	tableOutput("tbl_KmeansPromoter")
+		)
+	}
+)
 
 
+View.Kmeans$set("public", "PopNumberOfClusters",
+	function(){
+		bsModal(
+			"modal_Kmeans_ShowNumberOfClusters",
+			"Determining the number of clusters (k)", 
+			"btn_Kmeans_showNumberOfClusters",
+			size = "large",
+			h5("Following the elbow method, one should choose k so that adding another cluster 
+         		does not substantially reduce the within groups sum of squares." ,
+          		a(	"Wikipedia", 
+				  	href="https://en.wikipedia.org/wiki/Determining_the_number_of_clusters_in_a_data_set",
+          			target="_blank"
+				)
+			),
+			plotOutput("Kmeans_Nclusters")
+		)
+	}
+)
 
 
+View.Kmeans$set("public", "PopTSNEPlot",
+	function(){
+		bsModal(
+			"modal_Kmeans_TSNEPlot",
+			"t-SNE plot of genes",
+			"btn_Kmeans_ShowTSNE",
+			size = "large",
+			h5("We use the dimension reduction algorith ",
+				a("t-SNE", href="https://lvdmaaten.github.io/tsne/",target="_blank"), 
+				"to map the top genes. Examine the distribution can help choose the nubmer of clusters in k-Means. "
+			),
+			checkboxInput("is_Kmeans_ColorGenes", "Color genes by the results of k-Means", value = TRUE),
+			actionButton("btn_Kmeans_Recalculate", "Re-calculate using different random numbers"),
+			plotOutput("Kmeans_tSNEgenePlot")
+		)
+	}
+)
+
+View.Kmeans$set("public", "PopGeneDistribution",
+	function(){
+		bsModal(
+			"modal_Kmeans_GeneDistribution",
+			"Distribution of variations among genes",
+			"btn_Kmeans_ShowGeneSD",
+			size = "large",
+			downloadButton('download_Kmeans_GeneDistribution',"Download Figure"),
+			plotOutput("Kmeans_GeneDistribution")
+		)
+	}
+)
+
+View.Kmeans$set("public", "PopVisualizeEnrichment",
+	function(){
+		bsModal(
+			"modal_Kmeans_VisualizeEnrichment",
+			"Visualize enrichment",
+			"btn_Kmeans_ShowEnrichmentPlot",
+			size = "large",
+			h5("Gene sets closer on the tree share more genes. Sizes of dot correspond to adjuested Pvalues"),
+			downloadButton('download_Kmeans_EnrichmentPlot',"Download Figure"),
+			plotOutput('Kmeans_EnrichmentPlot')
+		)
+	}
+)
 
 
