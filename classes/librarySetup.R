a78b5d3e935f86557abf19c09176bdc20233ec30
Halforc Null <yaorunan@gmail.com>
2019-11-06 00:30:18 -0600

    org.Hs.ipi.db no longer support in new R version
    remove from bio package list

--- classes/librarySetup.R
+++ classes/librarySetup.R
@@ -17,7 +17,7 @@ list.of.bio.packages  <- c(
   "PREDAsampledata", "sfsmisc", "lokern", "multtest", "hgu133plus2.db", 
    "org.Ag.eg.db","org.At.tair.db","org.Bt.eg.db","org.Ce.eg.db","org.Cf.eg.db",
    "org.Dm.eg.db","org.EcK12.eg.db","org.EcSakai.eg.db","org.Gg.eg.db",
-   "org.Hs.eg.db","org.Hs.ipi.db","org.Mm.eg.db","org.Mmu.eg.db","org.Pf.plasmo.db",
+   "org.Hs.eg.db","org.Mm.eg.db","org.Mmu.eg.db","org.Pf.plasmo.db",
    "org.Pt.eg.db","org.Rn.eg.db","org.Sc.sgd.db","org.Ss.eg.db","org.Xl.eg.db"
 )
 
