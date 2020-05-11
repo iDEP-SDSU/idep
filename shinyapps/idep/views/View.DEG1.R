fced2ca98985d65bd45cc1220eb8c64390c42164
Halforc Null <yaorunan@gmail.com>
2020-05-11 01:55:14 -0500

    DEG1 UI done

--- shinyapps/idep/views/View.DEG1.R
+++ shinyapps/idep/views/View.DEG1.R
@@ -17,7 +17,8 @@ View.DGE1$set("public", "sidebarPanel",
             h5("Identifying Differential Expressed Genes (DEGs). See next tab for details."),
             self$Cond_DataIsReadCount(),
             self$Cond_DataIsNormalizedExpressionValues(),
-
+            self$OtherSettings(),
+            width = 4
         )
     }
 )
@@ -98,5 +99,80 @@ View.DEG1$set("public", "PopShowModelAndComparisons",
 )
 
 View.DEG1$set("public", "Cond_DataIsReadCount",
+    function(){
+        conditionalPanel(
+            "input.dataFileFormat == 1",
+            selectInput("CountsDEGMethod", "Method:", 
+                choices = list(
+                    "DESeq2"      = 3,
+                    "limma-voom"  = 2,
+                    "limma-trend" = 1
+                ),
+                selected = 3
+            ),       
+            tags$style(type='text/css', "#CountsDEGMethod { width:100%;   margin-top:-12px}")
+        )
+    }
 )
+
+View.DEG1$set("public", "Cond_DataIsNormalizedExpressionValues",
+    function(){
+        conditionalPanel( 
+            "input.dataFileFormat == 2", 
+            h5("Using the limma package")        
+        )
+    }
+)
+
+View.DEG1$set("public", "OtherSettings",
+    function(){
+
+        fluidRow(
+            column( 5,
+                numericInput("limmaPval",  # FDR cutoff
+                    label = h5("FDR cutoff"), 
+                    value = 0.1,
+                    min   = 1e-5,
+                    max   = 1,
+                    step  =.05
+                ) 
+            ),
+            column(7,
+                numericInput("limmaFC",   # fold change cutoff
+                    label = h5("Min fold change"),
+                    value = 2,
+                    min   = 1,
+                    max   = 100,
+                    step  = 0.5
+                ) 
+            )
+        ),
+        # style of limmaPval and limmaFC
+        tags$style(type='text/css', "#limmaPval { width:100%;   margin-top:-12px}"),
+        tags$style(type='text/css', "#limmaFC { width:100%;   margin-top:-12px}"),
+
+        actionButton("modelAndComparisons", "Select factors & comparisons"),
+        tags$head(tags$style("#modelAndComparisons{color: blue;font-size: 15px;}")),
+        br(),br(),
+
+        fluidRow(
+           column(6,actionButton("showVenn", "Venn Diagram") )
+        ),
+        br(),
+        fluidRow(          
+            column(8, downloadButton('downloadGeneListsGMT', 'Gene lists') ) 
+        ),
+        br(),
+        fluidRow(
+           column(8, downloadButton('download.DEG.data', 'FDR & fold-changes for all genes') )
+        ),
+        downloadButton('downloadSigGeneStats', 'Figure'),
+        br(),
+        h4( textOutput("textLimma") ),
+        tags$head(tags$style("#textLimma{color: blue;font-size: 15px;}"))  ,
+        a(h5("?",align = "right"), href="https://idepsite.wordpress.com/degs/",target="_blank")
+    }
+)
+
+
 
