7a50c23e033bdf0462459bad96844ca39c9b29e9
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-08-01 18:30:21 -0500

    Updating connection parameters and adding documentation on importing database

--- classes/DB-service.R
+++ classes/DB-service.R
@@ -2,6 +2,10 @@
 ## 1. sqlite3 convertIDs.db .dump > d96.db.bak
 ## 2. Manually edit data types
 ## 3. Import .bak file on PostgreSQL
+# (Alternative)
+## 1. brew install --HEAD pgloader
+## 2. createdb DB
+## 3. pgloader sqliteDB.db postgresql://USER:PASSWORD@HOST:PORT/DB
 
 # Installing drivers [on macOS]
 ## 1. brew install unixodbc
@@ -11,10 +15,10 @@
 # Create connection
 con <- DBI::dbConnect(odbc::odbc(),
                       driver   = "/usr/local/lib/psqlodbcw.so",
-                      database = "postgres",
-                      UID      = "postgres", #rstudioapi::askForPassword("Database user")
+                      database = "idep96",
+                      UID      = "idep", #rstudioapi::askForPassword("Database user")
                       PWD      = rstudioapi::askForPassword("Database password"),
-                      host     = "localhost", 
+                      server     = "pt.jacks.local", 
                       Port     = 5432)
 
 
