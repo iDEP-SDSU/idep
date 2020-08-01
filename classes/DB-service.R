63374674661b822d6992733f0bdf3da22266c265
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2020-08-01 18:41:47 -0500

    Porting DB operations from server.R

--- classes/DB-service.R
+++ classes/DB-service.R
@@ -21,6 +21,47 @@ con <- DBI::dbConnect(odbc::odbc(),
                       server     = "pt.jacks.local", 
                       Port     = 5432)
 
+convert <- con
+quotes <- DBI::dbGetQuery(convert, " select * from quotes")
+quotes <- paste0("\"", quotes$quotes, "\"", " -- ", quotes$author,".       ")
+
+# Create a list for Select Input options
+orgInfo <- DBI::dbGetQuery(convert, paste("select distinct * from orgInfo " ))
+orgInfo <- orgInfo[order(orgInfo$name), ]
+annotatedSpeciesCounts <- sort(table(orgInfo$group)) # total species, Ensembl, Plants, Metazoa, STRINGv10
+speciesChoice <- setNames(as.list(orgInfo$id), orgInfo$name2 )
+# add a defult element to list    # new element name       value
+
+speciesChoice <- append(setNames( "NEW","**NEW SPECIES**"), speciesChoice)
+speciesChoice <- append(setNames( "BestMatch","Best matching species"), speciesChoice)
+
+# move one element to the 2nd place
+move2 <- function(i) c(speciesChoice[1:2], speciesChoice[i], speciesChoice[-c(1, 2, i)])
+i <- which(names(speciesChoice) == "Vitis vinifera"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) == "Oryza sativa Japonica Group"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) == "Oryza sativa Indica Group"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) == "Glycine max"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) =="Zea mays"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) =="Arabidopsis thaliana"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) == "Saccharomyces cerevisiae"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice)  == "Caenorhabditis elegans"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice)  == "Drosophila melanogaster"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) =="Dog"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) =="Macaque"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) =="Chicken"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) =="Pig"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) =="Zebrafish" ); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) == "Cow" ); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) == "Rat" ); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) == "Mouse"); speciesChoice <- move2(i)
+i <- which(names(speciesChoice) == "Human"); speciesChoice <- move2(i)
+
+GO_levels <- DBI::dbGetQuery(convert, "select distinct id,level from GO  
+                                WHERE GO = 'biological_process'")
+level2Terms <- GO_levels[which(GO_levels$level %in% c(2,3)), 1]  # level 2 and 3
+idIndex <- DBI::dbGetQuery(convert, paste("select distinct * from idIndex "))
+quotes <- DBI::dbGetQuery(convert, " select * from quotes")
+quotes = paste0("\"", quotes$quotes, "\"", " -- ", quotes$author, ".       ")
 
 # References
 ## [1] Drivers: https://db.rstudio.com/best-practices/drivers/
