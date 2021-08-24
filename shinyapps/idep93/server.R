f005e9ef2e46489377dd0e1a7a09932fcb9d02e9
Roberto Villegas-Diaz <villegas.roberto@hotmail.com>
2021-08-24 20:48:37 +0100

    Attempt to read the data for the production server from the environment variable IDEP_DATA, if this variable is not found, then use "../../data/data100"

--- shinyapps/idep93/server.R
+++ shinyapps/idep93/server.R
@@ -91,7 +91,7 @@ STRING_DB_VERSION <- "11.0" # what version of STRINGdb needs to be used
 ################################################################
 
 # relative path to data files
-datapath = "../../data/data100/"   # production server
+datapath = Sys.getenv("IDEP_DATA", "../../data/data100/")   # production server
 
 sqlite  <- dbDriver("SQLite")
 convert <- dbConnect( sqlite, paste0(datapath, "convertIDs.db"), flags=SQLITE_RO)  #read only mode
