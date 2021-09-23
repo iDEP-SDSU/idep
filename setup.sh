c046cbca3ed6ef30c9168dd6b67a63dd4743123f
Ge <Xijin.Ge@sdstate.edu>
2021-09-23 09:58:31 -0500

    local install instruction

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
-# Based on digital ocean One click app: Docker-ce
+# Local install of iDEP and ShinyGO on a Linux server
+# requires docker, docker-compose
 
 docker build ./nginx/. -t nginx  #nginx image should be build very quick
 
@@ -8,8 +9,7 @@ docker build . -t webapp #webapp image need hours to build
 
 echo 'Docker images have been built. Start downloading data.'
 
-mkdir data
-cd data
+
 wget --no-check-certificate https://mft.sdstate.edu/public/file/3Y66fppA0Eym0G41taPtRw/data104.tar.gz .
 tar xvzf data104.tar.gz
 rm data104.tar.gz
