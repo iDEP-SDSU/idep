81f216859a0938602a9fc04067f11b04b89ca2ae
Steven Ge <xijin.ge@sdstate.edu>
2021-09-24 10:39:00 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -19,7 +19,11 @@ sudo docker tag gexijin/idep webapp
 echo 'Docker images have been built. Start downloading data.'
 
 # Download and unzip database files; this can take 2 hours
-wget --no-check-certificate https://mft.sdstate.edu/public/file/3Y66fppA0Eym0G41taPtRw/data104.tar.gz .
+# use Amazon server for faster download
+wget http://18.235.92.206:8080/data104.tar.gz 
+
+#slower server
+#wget --no-check-certificate https://mft.sdstate.edu/public/file/3Y66fppA0Eym0G41taPtRw/data104.tar.gz .
 tar xvzf data104.tar.gz
 rm data104.tar.gz
 
