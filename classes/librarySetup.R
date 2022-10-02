5e28721ee76c4520239b50b1fadda69e8d0d60c0
Steven Ge <xijin.ge@sdstate.edu>
2022-10-02 15:16:38 -0500

    Update librarySetup.R

--- classes/librarySetup.R
+++ classes/librarySetup.R
@@ -26,7 +26,7 @@ list.of.bio.packages  <- c(
   "PREDAsampledata", "sfsmisc", "lokern", "multtest", "hgu133plus2.db",
   "BiocParallel", "ComplexHeatmap", "InteractiveComplexHeatmap", "SummarizedExperiment",
   "impute", "preprocessCore", # required by WGCNA
-  "PCAtools"
+  "PCAtools",
   "org.Ag.eg.db","org.At.tair.db","org.Bt.eg.db","org.Ce.eg.db","org.Cf.eg.db",
   "org.Dm.eg.db", "org.Dr.eg.db", "org.EcK12.eg.db","org.EcSakai.eg.db","org.Gg.eg.db",
   "org.Hs.eg.db","org.Mm.eg.db","org.Mmu.eg.db","org.Pf.plasmo.db",
