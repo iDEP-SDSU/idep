f6ffe01c83cc215f26504f0317430158b6988bfa
Steven Ge <xijin.ge@sdstate.edu>
2023-02-27 19:37:49 -0600

    Restart docker dameon

--- restart.sh
+++ restart.sh
@@ -11,6 +11,8 @@ git checkout master
 git pull
 cd ../..
 
+# Restart docker dameon; sometimes it causes problem
+sudo systemctl restart docker
 #sudo docker system prune --volumes
 sudo docker-compose down 
 #free buffer/cache  https://unix.stackexchange.com/questions/87908/how-do-you-empty-the-buffers-and-cache-on-a-linux-system
