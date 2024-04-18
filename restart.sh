a7bc3f0347d7316f9503c3065e786ce5aee2a183
Steven Ge <xijin.ge@sdstate.edu>
2024-04-18 10:16:24 -0500

    25 containers

--- restart.sh
+++ restart.sh
@@ -19,4 +19,4 @@ sudo docker-compose down
 sudo sh -c 'echo 1 >/proc/sys/vm/drop_caches'
 sudo sh -c 'echo 2 >/proc/sys/vm/drop_caches'
 sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'
-sudo docker-compose up -d --scale webapp=30
+sudo docker-compose up -d --scale webapp=25
