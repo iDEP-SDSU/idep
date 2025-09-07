0c58baf40a2c5a8ad1737905b4152ff1d34a300b
Steven Ge <xijin.ge@sdstate.edu>
2025-09-07 16:26:34 -0500

    restart script

--- restart_server.sh
+++ restart_server.sh
@@ -1,22 +1,12 @@
-#git pull
 
-#git submodule update --init --recursive
-
-#cd shinyapps/go
-#git checkout master
-#git pull
-
-#cd ../idepgolem1
-#git checkout master
-#git pull
-#cd ../..
+sudo docker-compose down 
 
 # Restart docker dameon; sometimes it causes problem
 sudo systemctl restart docker
-#sudo docker system prune --volumes
-sudo docker-compose down 
+
 #free buffer/cache  https://unix.stackexchange.com/questions/87908/how-do-you-empty-the-buffers-and-cache-on-a-linux-system
 sudo sh -c 'echo 1 >/proc/sys/vm/drop_caches'
 sudo sh -c 'echo 2 >/proc/sys/vm/drop_caches'
 sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'
+
 sudo docker-compose up -d --scale webapp=25
