97d091dfbcfb0e21b7d80306bc6f755c5094c301
Steven Ge <xijin.ge@sdstate.edu>
2025-09-12 12:07:09 -0500

    idep v2.01 and v 1.13 as separate R packages

--- shinyapps/idep20/app.R
+++ shinyapps/idep20/app.R
@@ -4,4 +4,4 @@
 
 #pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
-idepGolem::run_app() # add parameters here (if any)
+idepGolem201::run_app() # add parameters here (if any)
