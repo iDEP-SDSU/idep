4310c446e23e31949138aa8f5fc21939005cb1e2
Steven Ge <xijin.ge@sdstate.edu>
2025-11-05 19:13:22 -0600

    disable JetStream2

--- restart_server.sh
+++ restart_server.sh
@@ -9,4 +9,4 @@ sudo sh -c 'echo 1 >/proc/sys/vm/drop_caches'
 sudo sh -c 'echo 2 >/proc/sys/vm/drop_caches'
 sudo sh -c 'echo 3 >/proc/sys/vm/drop_caches'
 
-sudo docker compose up -d --scale webapp=40
+sudo docker compose up -d --scale webapp=50
